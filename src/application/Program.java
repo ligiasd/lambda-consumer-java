9fbd528d9cc786ddee038364e65fb41ad84956c4
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2022-03-11 15:48:30 -0300

    add function lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,8 +16,13 @@ public class Program {
         list.add(new Product("Tablet",350.00));
         list.add(new Product("HD Case",80.90));
 
+        double factor = 1.1;
+        Consumer<Product> cons = p -> {
+            p.setPrice(p.getPrice() * factor);
+        };
+
 //        list.forEach(new PriceUpdate());
-        list.forEach(Product::noStaticPriceUpdate); //ref method to class
+        list.forEach(cons); //ref method to class
 
         list.forEach(System.out::println);
 
